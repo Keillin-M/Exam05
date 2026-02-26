15caaae7483439a51979ff527821d7d9c7a94c9f
Keillin Maeda <keillin.maeda@gmail.com>
2026-02-26 16:20:47 +0100

    Atualizar o vect21.cpp

--- vect2/vect21.cpp
+++ vect2/vect21.cpp
@@ -1,2 +1,33 @@
 #include "vect21.hpp"
 
+                 vect2(int x, int y);
+                 vect2(const vect2 &other);
+                 vect2 &operator=(const vect2 &other);
+
+vect2& operator++();
+vect2 operator++(int);
+vect2& operator--();
+vect2 operator--(int);
+
+vect2 operator+(const vect2 &other) const;
+vect2& operator+=(const vect2 &other);
+
+vect2 operator-(const vect2 &other) const;
+vect2& operator-=(const vect2 &other);
+
+vect2 operator*(const vect2 &other) const;
+vect2& operator*=(const vect2 &other);
+
+vect2 operator-(const vect2 &other) const;
+
+bool operator==(const vect2 &other) const;
+bool operator!=(const vect2 &other) const;
+
+int operator[](int index);
+int &operator[](int index) const;
+
+};
+
+vect2 operator*(int scalar, const vect2 &other);
+
+std::ostream &operator<<(std::ostream &s, const vect2 &v);
